8b392c5d1e9bdf46b90091f2826c91677f9d44e8
Oliver <olivercreativeworks@gmail.com>
2021-03-22 20:52:50 -0400

    switched first and second values

--- calc.py
+++ calc.py
@@ -1,9 +1,9 @@
 firstNum = int(input('Enter your first number: '))
 secondNum = int(input('Enter your second number: '))
 Addition = firstNum + secondNum
-Subtraction = firstNum - secondNum
+Subtraction = secondNum - firstNum
 Multiplication = firstNum * secondNum
-Division = firstNum / secondNum
+Division = secondNum/firstNum
 Modulo = firstNum % secondNum
 print(f'Addition: {Addition}\nSubtraction: {Subtraction}\nMultiplication: {Multiplication}\nDivision: {Division}\nModulo: {Modulo}')
  
